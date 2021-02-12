1e29bd7644f19e405ec7a6dc618abd251d7e6531
Warwick Stone <uozu.aho@gmail.com>
2021-02-13 10:45:44 +1100

    POC

---
+++ locustfile.py
@@ -0,0 +1,13 @@
+import time
+from locust import HttpUser, task, between
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2.5)
+
+    @task
+    def hello_world(self):
+        self.client.get("/")
+
+    @task(3)
+    def view_items(self):
+        self.client.get("/Privacy")
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Locust demo
+
+Runs locust against a dotnet server, using docker-compose.
+
+
+# Getting started
+
+- install docker
+
+```sh
+git clone https://github.com/dotnet/dotnet-docker
+docker-compose up -d
+```
+
+- browse to localhost:8089
+- start a load test!
