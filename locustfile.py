aa2505d4c07cec612a94f1f52f655bed02a47dae
Warwick Stone <uozu.aho@gmail.com>
2021-02-13 12:47:05 +1100

    locust: /work/[id] results grouped

--- locustfile.py
+++ locustfile.py
@@ -5,17 +5,12 @@ class WorkUser(HttpUser):
     wait_time = between(0.5, 0.5)
 
     @task
-    class DoWork(SequentialTaskSet):
+    def create_work(self):
+        with self.client.post("/work?message=yo") as create_response:
+            job_id = int(create_response.content)
 
-        @task
-        def create_work(self):
-            with self.client.post("/work?message=yo") as create_response:
-                print("created job:", create_response)
-                job_id = int(create_response.content)
-
-            work_complete = False
-            while not work_complete:
-                with self.client.get(f"/work/{job_id}") as poll_response:
-                    print("job status:", poll_response)
-                    work_complete = poll_response.content == b"done"
-                time.sleep(1)
+        work_complete = False
+        while not work_complete:
+            with self.client.get(f"/work/{job_id}", name="/work/[id]") as poll_response:
+                work_complete = poll_response.content == b"done"
+            time.sleep(1)
