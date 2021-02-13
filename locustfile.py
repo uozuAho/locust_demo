509c2bbe11df1f4a4cec4af33976b2fe45d9d21b
Warwick Stone <uozu.aho@gmail.com>
2021-02-13 14:34:04 +1100

    clean up locustfile

--- locustfile.py
+++ locustfile.py
@@ -2,23 +2,31 @@ import time
 from datetime import datetime, timedelta
 from locust import HttpUser, task, between, SequentialTaskSet
 
-class WorkUser(HttpUser):
+class CreateJobAndWaitUntilCompleted(HttpUser):
     @task
-    def create_work(self):
+    def doit(self):
+        job_id = self.start_job()
+        start_time = datetime.now()
+
+        self.wait_for_job_to_complete(job_id)
+        finish_time = datetime.now()
+
+        self.notify_work_completed(start_time, finish_time)
+
+    def start_job(self):
         with self.client.post("/work?message=yo") as create_response:
             job_id = int(create_response.content)
+            return job_id
 
-        work_started_time = datetime.now()
-
+    def wait_for_job_to_complete(self, job_id):
         work_complete = False
         while not work_complete:
             with self.client.get(f"/work/{job_id}", name="/work/[id]") as poll_response:
                 work_complete = poll_response.content == b"done"
             time.sleep(.25)
 
-        work_completed_time = datetime.now()
-
-        work_time_ms = (work_completed_time - work_started_time).total_seconds() * 1000
+    def notify_work_completed(self, start_time, finish_time):
+        work_time_ms = (finish_time - start_time).total_seconds() * 1000
 
         self.client.request_success.fire(
             request_type="",
