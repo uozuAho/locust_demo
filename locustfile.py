1dc04e7d83b10c73ed8dab771c59467191b6ba51
Warwick Stone <uozu.aho@gmail.com>
2021-02-13 12:55:00 +1100

    locust: record time taken to complete work

--- locustfile.py
+++ locustfile.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime, timedelta
 from locust import HttpUser, task, between, SequentialTaskSet
 
 class WorkUser(HttpUser):
@@ -9,8 +10,20 @@ class WorkUser(HttpUser):
         with self.client.post("/work?message=yo") as create_response:
             job_id = int(create_response.content)
 
+        work_started_time = datetime.now()
+
         work_complete = False
         while not work_complete:
             with self.client.get(f"/work/{job_id}", name="/work/[id]") as poll_response:
                 work_complete = poll_response.content == b"done"
             time.sleep(1)
+
+        work_completed_time = datetime.now()
+
+        work_time_ms = (work_completed_time - work_started_time).total_seconds() * 1000
+
+        self.client.request_success.fire(
+            request_type="",
+            name="work completed",
+            response_time=work_time_ms,
+            response_length=0)
